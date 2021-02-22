9f371560a7a216f6b2c54118204bfa37300af9a1
Uvin Weththasinghe <uvin6667@gmail.com>
2021-02-22 13:58:17 +0530

    Implement exception handling via pre-built exception handling files

---
+++ src/main/java/com/spil/web/dto/ErrorDTO.java
@@ -0,0 +1,49 @@
+package com.spil.web.dto;
+
+public class ErrorDTO {
+    private int status;
+    private String error;
+    private String message;
+
+    public ErrorDTO() {
+    }
+
+    public ErrorDTO(int status, String error, String message) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorDTO{" +
+                "status=" + status +
+                ", error='" + error + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/spil/web/exception/HttpResponseException.java
@@ -0,0 +1,19 @@
+package com.spil.web.exception;
+
+public class HttpResponseException extends RuntimeException {
+    private int statusCode;
+
+    public HttpResponseException(int statusCode, String message, Throwable cause) {
+        super(message, cause);
+        this.setStatusCode(statusCode);
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+}
